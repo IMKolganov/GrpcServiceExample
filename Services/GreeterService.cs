697aad36b7506bf195677a7e9cf04a7a1dbdb9f7
Ivan <110064602+IMKolganov@users.noreply.github.com>
2023-06-23 18:07:37 +0300

    add get unique big int

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -1,11 +1,14 @@
+using System.Numerics;
 using Grpc.Core;
 using GrpcServiceExample;
+using GrpcServiceExample.Services.Untils;
 
 namespace GrpcServiceExample.Services;
 
 public class GreeterService : Greeter.GreeterBase
 {
     private readonly ILogger<GreeterService> _logger;
+    private static readonly BigIntegerWrapper UniqueIdWrapper = new();
 
     public GreeterService(ILogger<GreeterService> logger)
     {
@@ -14,9 +17,13 @@ public class GreeterService : Greeter.GreeterBase
 
     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new HelloReply
+        lock (UniqueIdWrapper)
         {
-            Message = "Hello " + request.Name
-        });
+            UniqueIdWrapper.Value++;
+            return Task.FromResult(new HelloReply
+            {
+                Message = $"Hello {request.Name} your unique bigint value {UniqueIdWrapper.Value}"
+            });
+        }
     }
 }
---
+++ Services/Untils/BigIntegerWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace GrpcServiceExample.Services.Untils;
+
+public class BigIntegerWrapper
+{
+    public BigIntegerWrapper()
+    {
+    }
+
+    public BigIntegerWrapper(BigInteger value)
+    {
+        Value = value;
+    }
+
+    public BigInteger Value { get; set; } = BigInteger.Zero;
+}
